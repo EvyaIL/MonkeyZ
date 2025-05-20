1ee295a88df616c9fc20567b8ff87511a4de09d5
Zohar Klimovski <evyatarhypixel1477@gmail.com>
2025-05-21 02:11:39 +0300

    a

--- frontend/src/components/inputs/RangeInput.jsx
+++ frontend/src/components/inputs/RangeInput.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Slider from "rc-slider";
+import { Range } from "rc-slider";
 import "rc-slider/assets/index.css";
 import { useTranslation } from "react-i18next";
 
@@ -68,8 +68,7 @@ const RangeInput = ({ value, onChange }) => {
 
   return (
     <div className="w-full flex flex-col gap-4">
-      <Slider
-        range
+      <Range
         min={sliderMin}
         max={sliderMax}
         value={[value.min, value.max]}
